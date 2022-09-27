b8536c23e08634653bc20a3657092f23c8fa6938
Fisatidi Andrey <fissatidi@gmail.ru>
2022-09-27 22:33:18 +0500

    last2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import java.util.*;
 
 public class Main {
-    public static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-    public static final List<String> word = new ArrayList<>(List.of("лес", "лог", "лес", "поле", "лог"));
 
     public static void main(String[] args) {
-        firstTask();
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> word = new ArrayList<>(List.of("лес", "лог", "лес", "поле", "лог"));
+
+        firstTask(nums);
         fourthTask(word);
-        secondTask();
+        secondTask(nums);
         thirdTask(word);
     }
 
-    public static Integer firstTask() {
+    public static void firstTask(List<Integer> nums) {
         Set<Integer> num = new HashSet<>(nums);
         for (Integer x : num) {
             if (x % 2 != 0) {
@@ -19,38 +20,34 @@ public class Main {
             }
         }
         System.out.println();
-        return null;
     }
 
 
-    public static Integer secondTask() {
+    public static void secondTask(List<Integer> nums) {
         Set<Integer> num = new HashSet<>(nums);
-        Collections.sort(nums);
-        for (int x : num) {
+        TreeSet<Integer> numTree = new TreeSet<>(num);
+        for (int x : numTree) {
             if (x % 2 == 0) {
                 System.out.print(x);
             }
         }
-        System.out.println();
-        return null;
     }
 
-    public static String thirdTask(List<String> word) {
+    public static void thirdTask(List<String> word) {
         HashSet<String> words = new HashSet<>();
-        words.addAll(Main.word);
-        Main.word.clear();
-        Main.word.addAll(words);
+        words.addAll(word);
+        word.clear();
+        word.addAll(words);
         System.out.println(words);
-        return null;
     }
 
 
-    public static String fourthTask(List<String> word) {
-        Set<String> unique = new HashSet<>(Main.word);
-        System.out.println(Main.word.size() - unique.size());
-        return null;
+    public static void fourthTask(List<String> word) {
+        Set<String> unique = new HashSet<>(word);
+        System.out.println(word.size() - unique.size());
     }
 }
+
 
 
 
